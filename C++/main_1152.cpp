a02a3d6f3746beaf0e42dd646d216cee53ebfebf
wldndsla8@naver.com <wldndsla8@naver.com>
2017-06-28 21:31:33 +0900

    1152 단어의 개수

---
+++ C++/main_1152.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+/*
+input
+The Curious Case of Benjamin Button
+output
+6
+
+https://www.acmicpc.net/problem/1152
+*/
+
+using namespace std;
+
+int main(void)
+{
+	string str;
+	string temp;
+	int num = 0;
+	getline(cin, str);
+
+	stringstream stream(str);
+
+	while (stream >> temp) {
+		num++;
+	}
+
+	cout << num << endl;
+
+	return 0;
+}
