55234d18759c4afcdb1dc147e01803a2f4af15bf
wldndsla8@naver.com <wldndsla8@naver.com>
2017-12-17 16:17:51 +0900

    Dijstra

--- C++/main_1753(다익스트라).cpp
+++ C++/main_1753(다익스트라).cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 #include <queue>
 #include <map>
 #include <stack>
 #include <vector>
 
-#define MAX_VALUE -9999999
+#define MAX_VALUE -2147483647			// 우선순위 큐 사용을 위해(디폴트로 큰값을 뺌, 빠른 속도를 위해 compare를 오버 로딩x)
+
+using namespace std;
 
 /*
 input:
@@ -27,20 +30,16 @@ INF
 https://www.acmicpc.net/problem/1753
 /* Copyright (C) 2017 by Son */
 
-using namespace std;
-
+// 인덱스 무조건 1부터
 class Vertex {
 public:
 	int index;
 	int dist;
-	int post = 0;
-	Vertex(int index, int dist, int post) :index(index), dist(dist), post(post) {
+	Vertex() {}
+	Vertex(int index, int dist) :index(index), dist(dist) {
 	}
 	Vertex(int index) :index(index) {
 		dist = MAX_VALUE;
-	}
-	void setDist(int d) {
-		dist = d;
 	}
 	bool operator >(const Vertex& v) const {
 		return dist > v.dist;
@@ -51,103 +50,100 @@ public:
 };
 class Graph {
 public:
-	int n = 0;
-	vector<int> dist;
-	vector<vector<pair<int, int> > > adj;
+	int num = 0;
+	int* dist;
+	vector<Vertex> vt;
+	vector<pair<int, int>>* adj;
 	priority_queue<Vertex> pq;
-	map<int, pair<int, int> > m;
+	//map<int, pair<int, int>> m;			
 
-	void addVertex(int index, int sig) {
-		pq.push(Vertex(index, 0, index));
-		this->dist.push_back(0);
-		n++;
-		adj.resize(n + 1);
-	}
 	void addVertex(int index) {
-		pq.push(Vertex(index));
-		this->dist.push_back(-1 * MAX_VALUE);
-		n++;
-		adj.resize(n);
+		vt.push_back(Vertex(index));
+		num++;
+	}
+	void setAdj() {
+		adj = new vector<pair<int, int>>[num];
 	}
 	void addAdj(int u, int v, int w) {
 		adj[u - 1].push_back(make_pair(v, w));
 	}
 	void dijkstra(int s) {
-		m.insert(make_pair(s, make_pair(0, -1)));
+		dist = new int[num];
+		for (int i = 0; i < num; i++) {
+			dist[i] = -1 * MAX_VALUE;
+		}
+		dist[s] = 0;
+		vt[s].dist = 0;
+		pq.push(vt[s]);
+		//m[s+1] = make_pair(0, -1);
+
 		while (!pq.empty()) {
 			int index = pq.top().index - 1;
+			int temp = dist[index];
 			int cost = -1 * pq.top().dist;
 			pq.pop();
-			if (dist[index] < cost) {
+			if (temp < cost) {
 				continue;
 			}
-			int adj_size = adj[index].size();
-			for (int i = 0; i < adj_size; i++) {
+			int size = adj[index].size();
+			for (int i = 0; i < size; i++) {
 				int n = adj[index][i].first;
-				int v = adj[index][i].second;
-				if (dist[n - 1] > dist[index] + v) {
-					dist[n - 1] = dist[index] + v;
-					Vertex newVt = Vertex(n, -dist[n - 1], index + 1);
-					pq.push(newVt);
-					m[n] = make_pair(dist[n - 1], index + 1);
+				int v = adj[index][i].second + temp;
+				if (dist[n - 1] > v) {
+					dist[n - 1] = v;
+					pq.push(Vertex(n, -v));
+					//m[n] = make_pair(v, index+1);
 				}
 			}
 		}
 	}
 	void printIndexMinDis(int index) {
 		//인덱스별 최소거리
-		if (dist[index-1] != -1*MAX_VALUE) {
-			cout << dist[index - 1] << "\n";
-		}			
+		if (dist[index] != -1 * MAX_VALUE) {
+			cout << dist[index] << "\n";
+		}
 		else {
 			cout << "INF\n";
 		}
 	}
-	void printPostAndMinDis(int index) {
-		//printf("인덱스 - 최단 경로 - 이전 정점\n");
-		//printf("%d - %d - %d\n", index, m[index].first, m[index].second);
+	/*
+	void printMinRoute(int index) {
+	if (dist[index] != -1 * MAX_VALUE) {
+	stack<int> st;
+	int next = m[index].second;
+	while (next != -1) {
+	st.push(next);
+	next = m[next].second;
 	}
-	void printMinRoute(int index){
-		//printf(인덱스까지 경로 : ", index);
-		/*
-		if (dist[index-1] != -1*MAX_VALUE) {
-			stack<int> st;
-			int next = m[index].second;
-			while (next != -1) {
-				st.push(next);
-				next = m[next].second;
-			}
-			while (!st.empty()) {
-				printf("%d - ", st.top());
-				st.pop();
-			}
-			printf("%d\n", index);
-		}*/
+	while (!st.empty()) {
+	printf("%d - ", st.top());
+	st.pop();
 	}
+	printf("%d\n", index);
+	}
+	}*/
 };
-
-int main() {
+int main()
+{
 	std::ios::sync_with_stdio(false);
 	int i, n, m, s;
 	int u, v, w;
 	cin >> n >> m;
 	cin >> s;
 	Graph gp;
-	for (i = 1; i<=n; i++) {
-		if (i == s) {
-			gp.addVertex(i, 0);
-		}
-		else {
-			gp.addVertex(i);
-		}
+
+	for (i = 1; i <= n; i++) {
+		gp.addVertex(i);
 	}
+	gp.setAdj();
 	for (i = 0; i < m; i++) {
 		cin >> u >> v >> w;
 		gp.addAdj(u, v, w);
 	}
-	gp.dijkstra(s);
-	for (i = 1; i <= n; i++) {
+	gp.dijkstra(s - 1);
+	for (i = 0; i < n; i++) {
 		gp.printIndexMinDis(i);
 	}
+
 	return 0;
 }
